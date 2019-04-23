09b8a30a8f6855b693d133b18eea17869cbe7a52
26583 <43673592+26583@users.noreply.github.com>
2019-04-23 09:13:30 +0200

    Add files via upload

---
+++ Assets/AvoidBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvoidBehavior : MonoBehaviour
+{
+    CarController carController;
+    public GameObject canta;
+    public int speed;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        carController = GameObject.Find("canta").GetComponent<CarController>();
+
+        if (!carController.getBool()) {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            transform.LookAt(target);
+        }
+        if (carController.getBool()) {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+            transform.LookAt(target);
+        }
+    
+    }
+}
---
+++ Assets/CarController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public int speed;
+    private int r;
+    private bool pickUp = false;
+    public Camera fpsCam;
+    private float range = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) {
+            Shoot();
+        }
+
+        transform.position += transform.forward * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A)) {
+            r -= 1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            r += 1;
+        }
+        transform.rotation = Quaternion.Euler(0, r, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "car") {
+            Application.LoadLevel("GameOver");
+        }
+        if (other.gameObject.name == "arena") {
+            Application.LoadLevel("GameOver");
+        }
+        if (other.gameObject.name == "pickUp") {
+           // Application.LoadLevel("GameOver");
+            Destroy(other.gameObject);
+            pickUp = true;
+        }
+    }
+    public bool getBool()
+    {
+        return pickUp;
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)) {
+            if(hit.transform.name == "car") {
+                Destroy(hit.transform.gameObject);
+            }
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
